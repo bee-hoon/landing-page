37aa224495a9993eb147589d07b035e28cddddea
behoon <syauqeesy@gmail.com>
2023-04-25 17:26:04 +0700

    fix: using escaped char

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col p-24 bg-purple-200 ${inter.className}`}
     >
       <h2 className='text-blue-500 text-3xl mb-4'>Hello</h2>
-      <h1 className='text-blue-400 text-5xl mb-4'>I'm <span className='text-blue-500 font-semibold'>Ahmad Syauqi</span></h1>
+      <h1 className='text-blue-400 text-5xl mb-4'>I&apos;m <span className='text-blue-500 font-semibold'>Ahmad Syauqi</span></h1>
       <h2 className='text-blue-400 text-5xl mb-8'><span className='text-blue-500 font-semibold'>a </span>Software Engineer</h2>
     </main>
   )
