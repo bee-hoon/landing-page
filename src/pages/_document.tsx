8b80353cd8f90cdffc6e3d1c902a1a543b963497
behoon <syauqeesy@gmail.com>
2023-04-25 18:54:19 +0700

    fix: add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head title='Ahmad Syauqi' />
+      <Head>
+        <title>Ahmad Syauqi</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
