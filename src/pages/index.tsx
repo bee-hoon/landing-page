278589ebabf02ac1394a4640e0785ff2684a068a
behoon <syauqeesy@gmail.com>
2023-04-25 19:56:24 +0700

    fix: padding on medium screen and above

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col p-4 py-16 md:py-22 md:pb-10 bg-purple-200 ${inter.className}`}
+      className={`flex min-h-screen flex-col p-4 py-20 md:p-20 bg-purple-200 ${inter.className}`}
     >
       <h2 className='text-blue-500 text-3xl md:text-3xl mb-4'>Hello</h2>
       <h1 className='text-blue-400 text-4xl md:text-5xl mb-4'>I&apos;m <span className='text-blue-500 font-semibold'>Ahmad Syauqi</span></h1>
